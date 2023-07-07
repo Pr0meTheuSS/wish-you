147a396ee0bbd0e3a541bc25a59f41bc9004566b
Юрий <yr.olimpiev@gmail.com>
2023-07-07 12:49:05 +0700

    Реализовал обработчик сообщений с пожеланиями
    Реализовал обработчик /send-message маршрута,
    накинул шаблон для работы с токенами jwt.

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,3 +1,16 @@
+/*
+ * Project: I-wish-you-app
+ * Created Date: Wednesday, July 5th 2023, 7:00:28 pm
+ * Author: Olimpiev Y. Y.
+ * -----
+ * Last Modified:  yr.olimpiev@gmail.com
+ * Modified By: Olimpiev Y. Y.
+ * -----
+ * Copyright (c) 2023 NSU
+ *
+ * -----
+ */
+
 package main
 
 import (
@@ -27,30 +40,15 @@ type Runnable interface {
 	run() error
 }
 
+type Message struct {
+	Text string `json:"message"`
+}
+
 func (a App) run() error {
 	fmt.Printf(
 		"Service %s is running, bro, server is listening port %d\n",
 		a.configurations.serviceName,
 		a.configurations.port)
-
-	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-	// 	fmt.Fprintf(w, "Hello, friends, application is running...")
-	// })
-
-	// http.HandleFunc("/echo", func(w http.ResponseWriter, r *http.Request) {
-	// 	fmt.Fprintf(w, r.URL.Query().Get("message"))
-	// })
-
-	// http.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
-	// 	body, err := ioutil.ReadFile("cmd/pages/login-page/login.html")
-	// 	if err != nil {
-	// 		log.Fatalf("unable to read file: %v", err)
-	// 	}
-	// 	fmt.Println(string(body))
-	// 	fmt.Fprintf(w, string(body))
-	// })
-
-	// return http.ListenAndServe("127.0.0.1:6969", nil)
 
 	router := gin.Default()
 
@@ -59,8 +57,32 @@ func (a App) run() error {
 		if err != nil {
 			log.Fatalf("unable to read file: %v", err)
 		}
-		fmt.Println(string(body))
 		ctx.Data(http.StatusOK, "text/html; charset=utf-8", body)
+	})
+
+	router.GET("/", func(ctx *gin.Context) {
+		body, err := ioutil.ReadFile("cmd/pages/main-page/main.html")
+		if err != nil {
+			log.Fatalf("unable to read file: %v", err)
+		}
+		ctx.Data(http.StatusOK, "text/html; charset=utf-8", body)
+	})
+
+	router.POST("/send-message", func(ctx *gin.Context) {
+		// Получение тела запроса в виде структуры Message
+		var message Message
+		if err := ctx.ShouldBindJSON(&message); err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Неверный формат данных"})
+			return
+		}
+
+		// Использование текста сообщения
+		fmt.Println("Текст сообщения:", message.Text)
+
+		// Ваша логика обработки сообщения
+
+		// Ответ сервера
+		ctx.JSON(http.StatusOK, gin.H{"status": "success"})
 	})
 
 	router.POST("/login", func(ctx *gin.Context) {
@@ -74,10 +96,6 @@ func (a App) run() error {
 		fmt.Printf("Handle post request for /login\n [username]: %s\t [password]: %s\n", username, password)
 
 		ctx.Data(http.StatusOK, "text/html; charset=utf-8", body)
-	})
-
-	router.GET("/", func(ctx *gin.Context) {
-
 	})
 
 	return router.Run(":" + strconv.Itoa(a.configurations.port))
